9ee6ad5e3e67c2949b5bf0b390cf2a3f2aad0fcc
keenan560 <keenan560@gmail.com>
2019-07-01 09:44:42 -0400

    got do-what-it-says to work but spotify-this won't work until auth. issue is fixed

--- liri.js
+++ liri.js
@@ -1,17 +1,15 @@
 require("dotenv").config();
 
+var fs = require('fs');
+
 var Spotify = require('node-spotify-api');
 
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
-console.log(Spotify);
+
 
 // var token = "BQDliFD1am7AHsYgTuA9rk3kxJ4cb7HGpDpN73LJMPeX3Abda4JsH2fJ1tPoqb0AQrklSud9e0JNgT9M9NsWCcpe631Osaa0LUBeGW-vfngci7nHiq8hcta4qelVjADVr0qtNi3fnplJi74BJ8TVQR7TGI0aMt0RmlURSK7L6d4tYmI";
-
-
-
-console.log(spotify);
 
 
 const axios = require('axios');
@@ -59,14 +57,14 @@ function concertThis(userInput) {
 
 function spotifyThisSong(userInput) {
 
-    spotify
-        .search({ type: 'track', query: userInput })
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (err) {
-            console.log(err);
-        });
+    spotify.search({ type: 'track', query: userInput }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(data);
+    });
+
 
 }
 
@@ -118,5 +116,25 @@ function movieThis(userInput) {
 }
 
 function doWhatItSays(userInput) {
-    console.log("Do it now!");
+    fs.readFile('random.txt', 'utf8', function (err, data) {
+        if (err) throw err;
+        // console.log(data);
+        var breakUp = data.split(",");
+        userCommand = breakUp[0];
+        userInput = breakUp[1];
+       
+        switch (userCommand) {
+            case "concert-this":
+                concertThis(userInput);
+                break;
+            case "spotify-this-song":
+                spotifyThisSong(userInput);
+                break;
+            case "movie-this":
+                movieThis(userInput);
+                break;
+        }
+
+    });
 }
+
